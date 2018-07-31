a74b4257199e271dd7ac60c3002bf83f34f56b16
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-07-31 15:36:19 +0530

    aadding  issues book  changes
    aadding  issues book  changes

--- application/models/Librarian_model.php
+++ application/models/Librarian_model.php
@@ -16,17 +16,19 @@ class Librarian_model extends CI_Model
 	}
 	public function get_saved_user(){
 		$this->db->select('*')->from('books_list');
-		
 		return $this->db->get()->result_array();
 		
 	}
-	 public function get_saved_user_edit($b_id){
-			   //echo'<pre>';print_r($b_id);exit;
-			   $this->db->select('*')->from('books_list');
+	public function get_saved_user_edit($b_id){
+		 $this->db->select('*')->from('books_list');
 		$this->db->where('b_id',$b_id);
 		return $this->db->get()->row_array();
-		  
-		   }
+	 }
+	 
+	 public  function save_issued_book($data){
+		 $this->db->insert('issued_book',$data);
+		 return $this->db->insert_id();
+	 }
 	
 	
 	
