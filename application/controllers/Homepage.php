0fe48b91e4c6873d9579c4dc11054551fb12a305
sivaksr <40159306+sivaksr@users.noreply.github.com>
2018-09-04 17:03:46 +0530

    home page controller
    home

---
+++ application/controllers/Homepage.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+@include_once( APPPATH . 'controllers/In_frontend.php');
+class Homepage extends In_frontend {
+public function __construct() 
+	{
+		parent::__construct();	
+			$this->load->model('Transportation_model');
+			$this->load->model('Student_model');
+	}
+	public function index()
+	{	
+		if($this->session->userdata('userdetails'))
+		{
+			
+					$this->load->view('page/index');
+					$this->load->view('html/footer');
+				}else{
+						$this->session->set_flashdata('error',"you don't have permission to access");
+						redirect('dashboard');
+				}
+		
+	
+	}
+	
+	public function program()
+	{	
+		if($this->session->userdata('userdetails'))
+		{
+			
+					$this->load->view('page/program-content');
+					$this->load->view('html/footer');
+				}else{
+						$this->session->set_flashdata('error',"you don't have permission to access");
+						redirect('dashboard');
+				}
+		
+	
+	}
+}	
