e85d535975a0be34155f12c6f2bbe0b3985484cb
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-09-04 17:06:38 +0530

    adding  url
    adding  url

--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -130,14 +130,16 @@
 				
 				<div class="col-md-12 mar-10-sm">
 					<div class="row">
+						<a href="<?php echo base_url('homepage/program'); ?>">
 						<div class="col-md-2">
-								<div class="<?php echo base_url('Homepage/program'); ?>">
+								<div class="card indigo lighten-1 text-center z-depth-2">
 									<div class="card-body">
 										<p class="white-text mb-0">Student/ Parent Login
 										</p>
 									</div>
 								</div>
 						</div>
+						</a>
 						<div class="col-md-2">
 								<div class="card indigo lighten-1 text-center z-depth-2">
 									<div class="card-body">
