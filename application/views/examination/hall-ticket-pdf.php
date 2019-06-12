5897b54ccde07a27e95ec6a4a84d66b4c60bf81d
bayapucg <bayapu.edu004@gmail.com>
2019-06-12 11:36:14 +0530

    commt
    cahnges

--- application/views/examination/hall-ticket-pdf.php
+++ application/views/examination/hall-ticket-pdf.php
@@ -1,228 +1,211 @@
-
-<div class="content-wrapper">
-    <section class="content">
-        <div class="row">
-            <!-- left column -->
-            <div class="col-md-12">
-                <!-- general form elements -->
-
-                <div class="box box-primary">
-                    
-
-                    <!-- /.box-header -->
-                    <!-- form start -->
-                    <div style="padding:20px;">
-                        
-                        
-						<?php if(isset($hall_ticket)&& count($hall_ticket)>0){?>
-                        <div class="attentdence-table" id="">
-						 
-                            <h2 class="text-center">Student Hall Ticket</h2><br>
-                            <div class="col-md-12">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <label class=" control-label">Roll Number</label>
-                                            <div class="">
-                                                <input class="form-control" name="" id="" value="<?php echo isset($hall_ticket['roll_number'])?$hall_ticket['roll_number']:''?>" disabled>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <label class=" control-label">Class</label>
-                                            <div class="">
-                                                <input class="form-control" name="" id="showHallticket" value="<?php echo isset($hall_ticket['name'])?$hall_ticket['name']:''?>" disabled>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <label class=" control-label">Section</label>
-                                            <div class="">
-                                                <input class="form-control" name="" id="" value="<?php echo isset($hall_ticket['section'])?$hall_ticket['section']:''?>" disabled>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <div class="form-group">
-                                            <label class=" control-label">Candidate's Name</label>
-                                            <div class="">
-                                                <input class="form-control" name="" id="" value="<?php echo isset($hall_ticket['student_name'])?$hall_ticket['student_name']:''?>" disabled>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <label class=" control-label">Gender</label>
-                                            <div class="">
-                                                <input class="form-control" name="" id="" value="<?php echo isset($hall_ticket['gender'])?$hall_ticket['gender']:''?>" disabled>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <div class="form-group">
-                                            <label class=" control-label">Father's Name</label>
-                                            <div class="">
-                                                <input class="form-control" name="" id="" value="<?php echo isset($hall_ticket['parent_name'])?$hall_ticket['parent_name']:''?>" disabled>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!--<div class="col-md-6">
-                                        <div class="form-group">
-                                            <label class=" control-label">Mother's Name</label>
-                                            <div class="">
-                                                <input class="form-control" name="" id="" value="Hey!" disabled>
-                                            </div>
-                                        </div>
-                                    </div>-->
-                                </div>
-                            </div>
-
-                            <div class="clearfix">&nbsp;</div>
-
-                            <div class="" style="padding:20px;">
-                                <div class="table-responsive">
-                                    <table id="" class="table table-bordered table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th class="text-center">Subject</th>
-                                                <th class="text-center">Date</th>
-                                                <th class="text-center">Time</th>
-                                                <th class="text-center">Invigilator Sign</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td><?php echo isset($hall_ticket['subject'])?$hall_ticket['subject']:'' ?></td>
-                                                <td><?php echo isset($hall_ticket['exam_date'])?$hall_ticket['exam_date']:'' ?></td>
-                                                <td><?php echo isset($hall_ticket['start_time'])?$hall_ticket['start_time']:'' ?>&nbsp; to&nbsp;<?php echo isset($hall_ticket['to_time'])?$hall_ticket['to_time']:'' ?></td>
-                                                <td></td>
-                                            </tr>
-                                           
-                                            
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                            
-                            <div class="col-md-12">
-                                <p>NOTE :</p>
-                                <?php if(isset($exam_instructions)&& count($exam_instructions)>0){ ?>
-								<?php  foreach($exam_instructions as $li){?>
-                                
-                                    <li><?php echo isset($li['instructions'])?$li['instructions']:''?></li>
-                               
-								<?php }?>
-								<?php }?>
-                            </div>
-                            
-                            <div class="col-md-4 pull-right">
-                                <div class="form-group">
-                                    <label class="text-center control-label">Principal Signature</label>
-                                    <div class="">
-                                        <input class="form-control" name="" id="" value="">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="clearfix">&nbsp;</div>
-                        </div>
-						<?php } ?>
-                    </div>
-                </div>
-                <!-- /.box -->
-            </div>
-        </div>
-        <!--/.col (right) -->
-    </section>
-</div>
-
-
-<script>
-$(document).ready(function(){
-	$("#HTFormat").css("display", "none");
-    $("#showHallticket").click(function(){
-        $("#HTFormat").css("display", "block");
-    });
-});
-</script>
-<script type="text/javascript">
-  $(document).ready(function(){
-    $("#attentdence").click(function(){
-        $(".attentdence-table").toggle();
-    });
-});
-  
-  </script>
-<script>
-function get_student_list(class_id){
-	if(class_id !=''){
-		    jQuery.ajax({
-   			url: "<?php echo base_url('Examination/class_student_list');?>",
-   			data: {
-				class_id: class_id,
-			},
-   			type: "POST",
-   			format:"Json",
-   					success:function(data){
-						
-						if(data.msg=1){
-							var parsedData = JSON.parse(data);
-						//alert(parsedData.list.length);
-							$('#student_id').empty();
-							$('#student_id').append("<option>select</option>");
-							for(i=0; i < parsedData.list.length; i++) {
-								//console.log(parsedData.list);
-							$('#student_id').append("<option value="+parsedData.list[i].u_id+">"+parsedData.list[i].name+"</option>");                      
-                    
-								
-							 
-							}
-						}
-						
-   					}
-           });
-	   }
-}
-
-</script>
-<script type="text/javascript">
-$(document).ready(function() {
-   $('#defaultForm').bootstrapValidator({
-//     
-        fields: {
-            exam_type: {
-                validators: {
-                    notEmpty: {
-                        message: 'Exam Type is required'
-                    }
-                }
-            },
-			class_id: {
-                validators: {
-                    notEmpty: {
-                        message: 'Class is required'
-                    }
-                }
-            },
-			student_id: {
-                validators: {
-                    notEmpty: {
-                        message: 'Student is required'
-                    }
-                }
-            }
-			
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>care hospitals</title>
+    
+    <style>
+    .invoice-box {
+        max-width: 800px;
+        margin: auto;
+        padding: 30px;
+        border: 1px solid #eee;
+        box-shadow: 0 0 10px rgba(0, 0, 0, .15);
+        font-size: 16px;
+        line-height: 24px;
+        font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+        color: #555;
+    }
+    
+    .invoice-box table {
+        width: 100%;
+        line-height: inherit;
+        text-align: left;
+    }
+    
+    .invoice-box table td {
+        padding-left: 5px;
+        vertical-align: top;
+    }
+    
+    <!-- .invoice-box table tr td:nth-child(2) { -->
+        <!-- text-align: right; -->
+    <!-- } -->
+    
+    .invoice-box table tr.top table td {
+        padding-bottom: 0px;
+    }
+    
+    .invoice-box table tr.top table td.title {
+        font-size: 45px;
+        line-height: 45px;
+        color: #333;
+    }
+    
+    .invoice-box table tr.information table td {
+        padding-bottom: 40px;
+    }
+    
+    .invoice-box table tr.heading td {
+        background: #eee;
+        border-bottom: 1px solid #ddd;
+        font-weight: bold;
+    }
+    
+    .invoice-box table tr.details td {
+        padding-bottom: 20px;
+    }
+    
+    .invoice-box table tr.item td{
+        border-bottom: 1px solid #eee;
+    }
+    
+    .invoice-box table tr.item.last td {
+        border-bottom: none;
+    }
+    
+    .invoice-box table tr.total td:nth-child(2) {
+        border-top: 2px solid #eee;
+        font-weight: bold;
+    }
+	
+    
+    @media only screen and (max-width: 600px) {
+        .invoice-box table tr.top table td {
+            width: 100%;
+            display: block;
+            text-align: center;
         }
-    });
+        
+        .invoice-box table tr.information table td {
+            width: 100%;
+            display: block;
+            text-align: center;
+        }
+    }
+    
+    /** RTL **/
+    .rtl {
+        direction: rtl;
+        font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+    }
+    
+    .rtl table {
+        text-align: right;
+    }
+    
+    .rtl table tr td:nth-child(2) {
+        text-align: left;
+    }
 
-    // Validate the form manually
-    $('#validateBtn').click(function() {
-        $('#defaultForm').bootstrapValidator('validate');
-    });
+    </style>
+</head>
 
-    $('#resetBtn').click(function() {
-        $('#defaultForm').data('bootstrapValidator').resetForm(true);
-    });
-});
-</script>
+<body>
+    <div class="invoice-box">
+        <table cellpadding="0" cellspacing="0">
+            <tr class="top">
+                <td colspan="2">
+                    <table>
+                        <tr>
+                            <td  style="width:180px;" class="title">
+                                <img src="https://s3.ap-south-1.amazonaws.com/edyooprod/media/institution/avatar/Sri_chaitanya_techno_school.jpg" style="width:auto; max-height:100px;">
+                            </td>
+                            
+                            <td style="text-align:left">
+                               <h1 >sri chaitanya school -Hall Ticket</h1>
+							   <p>Plot No. 3 & 4, Beside IBP Petrol Bunk, Sardar Patel Nagar, Opposite, Nizampet Cross Roads, Hyderabad, Telangana 500085</p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+		<table cellpadding="0" cellspacing="0" style="border:1px solid #aaa;border-bottom:0px solid #fff;">
+            <tr >
+                <td colspan="2" style="text-align:center;" >
+                  <h4 style="text-decoration:underline">Anual Examnations</h4>
+                </td>
+            </tr> 
+			<tr class="top">
+                <td>
+				<table>
+                  <tr>
+						<td>Name </td>
+						<th>: Chandrahas Reddy</th>
+				  </tr>
+				  <tr>
+						<td>Father Name </td>
+						<th>: Subba reddy</th>
+				  </tr> 
+				  <tr>
+						<td>Classs </td>
+						<th>: V - Section: B</th>
+				  </tr>
+				  <tr>
+						<td>Roll No </td>
+						<th>: C1100122</th>
+				  </tr> 
+				  </table>
+                </td>
+				 <td>
+				<table>
+                  <tr>
+						<td><img  style="width:100px;" src="https://media.karousell.com/media/photos/products/2017/04/04/id_photo_maker_passport_photostudent_card_photo_1491301933_67f72906.jpg"> </td>
+						
+				  </tr>
+				  </table>
+                </td>
+            </tr>
+        </table>
+		<table cellpadding="0" cellspacing="0" style="border:1px solid #aaa;border-bottom:0px solid #fff;">
+           
+			<tr class="top">
+                <td><strong>Date of Birth </strong> : 25/05/2000</td>
+                <td><strong>Medium </strong>: English</td>
+                <td><strong>Sex </strong> : Male</td>
+            </tr>
+        </table>
+		<table cellpadding="0" cellspacing="0" style="border:1px solid #aaa;border-bottom:0px solid #fff;">
+             <tr >
+                <td colspan="2" style="text-align:center;" >
+                  <h4 style="text-decoration:underline">Time Table</h4>
+                </td>
+            </tr> 
+			
+        </table>
+		<table class="table-sheet" style="border-collapse: collapse;">
+             <tr style=" border: 1px solid black;">
+                <th style=" border: 1px solid black;">Subject</th>
+                <th style=" border: 1px solid black;">Timings</th>
+                <th style=" border: 1px solid black;">English</th>
+                <th style=" border: 1px solid black;">Mathamatics</th>
+                <th style=" border: 1px solid black;">Science</th>
+                <th style=" border: 1px solid black;">Social</th>
+            </tr> 
+			<tr style=" border: 1px solid black;" >
+                <th style=" border: 1px solid black;">Paper 1</th>
+				<td style=" border: 1px solid black;">1030AM - 12.30 PM</td>
+				<td style=" border: 1px solid black;">25-05-2019</td>
+				<td style=" border: 1px solid black;">25-05-2019</td>
+				<td style=" border: 1px solid black;">25-05-2019</td>
+				<td style=" border: 1px solid black;">25-05-2019</td>
+			
+            </tr> 	
+			<tr style=" border: 1px solid black;" >
+                <th style=" border: 1px solid black;">Paper 2</th>
+				<td style=" border: 1px solid black;">1030AM - 12.30 PM</td>
+				<td style=" border: 1px solid black;">25-05-2019</td>
+				<td style=" border: 1px solid black;">25-05-2019</td>
+				<td style=" border: 1px solid black;">25-05-2019</td>
+				<td style=" border: 1px solid black;">25-05-2019</td>
+				
+            </tr> 
+			
+        </table>
+		
+		
+		
+    </div>
+</body>
+</html>
